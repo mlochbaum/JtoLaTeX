mlochbaum/JtoLaTeX

commit 3fff2d33d887242f4edcbe89cb6135c6262631d7

Correct for alias in processerror

jlatex.ijs
@@ -90,9 +90,10 @@ processerror =: 3 : 0
 err =. (}.~ 5+i.&LF) err1 =. }:13!:12 ''
 i =. >: i.&' '@:(#~ 2>:/\0,' '&=) err
 mmy =. mergeslash@:mergesparens ;:y
-mmy1 =. tofunc@:alias@:protectassign mmy
+mmy1 =. tofunc@:alias mmyp =. protectassign mmy
 if. err -.@-:&(-.&(LF,' ')) ;mmy1 do. err1 return. end.
 i =. i I.~ ([: +/\ +/@:~:&' '@>) mmy1
+i =. i I.~ +/\ aliaslen mmyp
 i =. (i{+/\0,+/@:~:&' '@> mmy) { ~.(i.@# + ' '&=) y
 (({.~ 5+i.&LF) err1) , i ({.,(4#' '),}.) y
 )

opdata.ijs
@@ -95,3 +95,4 @@ ALIAS =: (1 ({.,&<}.) ;:);._2 ]0 :0
 |  (| : jmodulus)
 )
 alias =: rplc&(,ALIAS1,ALIAS)
+aliaslen =: ((_2 (,<@#)&>/\ ,ALIAS1,ALIAS) boxtomapd 1)@>

commit 526bd15b23a05b0302ad5becd472706d9b4c0a00

Fix bug in error processing (introduced in 3fff2d33)

jlatex.ijs
@@ -93,7 +93,7 @@ mmy =. mergeslash@:mergesparens ;:y
 mmy1 =. tofunc@:alias mmyp =. protectassign mmy
 if. err -.@-:&(-.&(LF,' ')) ;mmy1 do. err1 return. end.
 i =. i I.~ ([: +/\ +/@:~:&' '@>) mmy1
-i =. i I.~ +/\ aliaslen mmyp
+i =. i I.~ 0,+/\ aliaslen mmyp
 i =. (i{+/\0,+/@:~:&' '@> mmy) { ~.(i.@# + ' '&=) y
 (({.~ 5+i.&LF) err1) , i ({.,(4#' '),}.) y
 )